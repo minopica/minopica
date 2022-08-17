f7dbab01100c4a163a27d16125f338d409eede2f
Antimo Pica <antimo.pica@windtre.it>
2022-08-17 18:49:50 +0200

    aggiunte configurazioni per report mocha

--- CypressAutomation/cypress.config.js
+++ CypressAutomation/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   defaultCommandTimeout: 5000,
   pageLoadTimeout: 30000,
   projectId: "2iw7j1",
+  reporter: 'mochawesome',
   
   env: {
     url: 'https://rahulshettyacademy.com',
