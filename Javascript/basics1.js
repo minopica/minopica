fd4329d1ea47ac88c54a81da8fe9eb9b81d3ad76
Antimo Pica <antimo.pica@windtre.it>
2022-08-25 00:59:39 +0200

    es javascriot

--- Javascript/basics1.js
+++ Javascript/basics1.js
@@ -5,3 +5,23 @@ console.log("Hello World")
 line1
 line2
 */
+
+let a = 4
+console.log(typeof(a))
+
+let b = 5
+
+console.log(a+b)
+
+let c = "pippo"
+console.log(typeof(c))
+
+c= a+b
+
+console.log(c)
+
+const flag = true
+console.log(typeof(flag))
+
+
+
---
+++ Javascript/basics2.js
@@ -0,0 +1,68 @@
+const flag = true
+
+if(!flag)
+{
+    console.log("condition satisfied")
+}
+else
+{
+    console.log("condition not satisfied")
+}
+
+let i=0
+while(i>10)
+{
+    i++
+    console.log(i)
+}
+
+do
+{
+    i++
+}
+while(i>10);
+console.log(i)
+
+
+for (let k=0; k<=10; k++)
+{
+    console.log(k)
+}
+
+
+let required=true
+while(required)
+{
+    console.log(required)
+    required=false
+}
+
+
+for (let k = 1; k<=10; k++)
+{
+    if(k%2 == 0 && k%5 == 0)
+    {
+        console.log(k)
+    }
+}
+
+for (let k = 1; k<=10; k++)
+{
+    if(k%2 == 0 || k%5 == 0)
+    {
+        console.log(k)
+    }
+}
+
+let n=0
+for (let k = 1; k<=100; k++)
+{
+    if(k%2 == 0 && k%5 == 0)
+    {
+        n++
+        console.log(k)
+        if (n==3) 
+            break
+    }
+}
+
