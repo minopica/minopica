a3e8177a745ddf7237b95d9d7be433d5da216bf3
Antimo Pica <antimo.pica@windtre.it>
2022-08-25 18:41:10 +0200

    test eredità classi

---
+++ Javascript/basics10.js
@@ -0,0 +1,6 @@
+
+import Pet from './basics9.js'
+
+let pet = new Pet("Sam","Sam")
+pet.fullName()
+console.log(pet.location)
---
+++ Javascript/basics9.js
@@ -0,0 +1,23 @@
+//inheritance is the main pillar in Object Oriented programming
+//once class can inherit/acquire all the properties/methods of another class
+//the class which inherits the properties of other is known as subclass (derived class, child class)
+//the class whose properties are inherited is known as superclass (or parent class)
+import Person from "./basics7.js"
+
+export default class Pet extends Person
+{
+    //same constructor of superclass -> call parent class constructor
+    constructor(firstName,lastName)
+    {
+        super(firstName,lastName)
+    }
+
+    get Location()
+    {
+        return "BlueCross"
+    }
+}
+
+// let pet = new Pet("Sam","Sam")
+// pet.fullName()
+// console.log(pet.location)
