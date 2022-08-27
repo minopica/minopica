cd486938ce51052e392c38510db63453c47016e3
Antimo Pica <antimo.pica@windtre.it>
2022-08-27 20:31:04 +0200

    sistemato test ricarica pwa cancel

--- CypressPWA/cypress/e2e/pwa/Ricarica_cancel_bs.js
+++ CypressPWA/cypress/e2e/pwa/Ricarica_cancel_bs.js
@@ -2,7 +2,7 @@
 
 describe('PWA test - Ricarica', function()
 {
-it('Ricarica - controllo webview SIA per inserimento CDC - env BS', function()
+it('Ricarica - controllo chiamata cancel in webview SIA al back da inserimento CDC - env BS', function()
 {
     Cypress.config('defaultCommandTimeout', 10000)
     cy.visit("https://priv:P3rz0nal!@pwa.bs.windtre.it/oa/auth/login")
@@ -17,7 +17,19 @@ it('Ricarica - controllo webview SIA per inserimento CDC - env BS', function()
     cy.contains("Carta di credito").click()
     cy.get('.execute-submit').click()
     cy.get('input[placeholder="Nome e cognome intestatario"]').type("Antimo")
+    
+    cy.intercept({
+        method : "POST",
+        path: "/api/v1/ch-payment-gw/topup/CREDIT_CARD/cancel"
+    }
+    ).as('requestCancel')
+
     cy.get('#back-button').click()
+    cy.wait('@requestCancel').should(({request,response}) =>
+    {
+        expect(response.statusCode).to.equal(200)
+    })
+ 
 
 }
 )
