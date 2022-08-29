c2cc16618165a8c64f65d9f93e807793525f027d
Antimo Pica <antimo.pica@windtre.it>
2022-08-30 01:27:25 +0200

    iframe winday

---
+++ CypressPWA/cypress/e2e/pwa/winday_registrato.js
@@ -0,0 +1,40 @@
+/// <reference types="Cypress" />
+/// <reference types="Cypress-iframe" />
+import 'cypress-iframe'
+import LoginPage from '../../support/pageObjects/LoginPage.js'
+
+describe('PWA test - Winday', function()
+{
+    const loginPage = new LoginPage()
+
+    before(() => {
+        // runs once before all tests
+        cy.fixture('example.json').then(function(data)
+        {
+            this.data = data
+        }
+        )
+
+      })
+
+it('Winday linea registrata', function()
+{
+    Cypress.config('defaultCommandTimeout', 10000)
+    cy.visit(Cypress.env("url_bs"))
+    loginPage.getEntraButton().click()
+    loginPage.getUsernameTextField().type("3931113321@example.com")
+    loginPage.getContinuaButton().click()
+    loginPage.getPasswordTextField().type('12345678')
+    loginPage.getAccediButton().click()
+    cy.get("img[alt='winday']").click()
+    // switch to iframe
+    cy.frameLoaded('iframe[title="Windtre"]')
+    cy.iframe().contains('GIOCA')
+    // cy.get('iframe[title="Windtre"]').its('0.contentDocument.body').contains('GIOCA')
+    // cy.wait(4000)
+}
+)
+
+
+}
+)
