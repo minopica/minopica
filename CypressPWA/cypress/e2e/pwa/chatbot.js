003da021232dc0547637fce651fa63c8ad953442
Antimo Pica <antimo.pica@windtre.it>
2022-08-30 08:10:36 +0200

    aggiunto test chatbot

---
+++ CypressPWA/cypress/e2e/pwa/chatbot.js
@@ -0,0 +1,42 @@
+/// <reference types="Cypress" />
+/// <reference types="Cypress-iframe" />
+import 'cypress-iframe'
+import LoginPage from '../../support/pageObjects/LoginPage.js'
+
+describe('PWA test - Chatbot', function()
+{
+    const loginPage = new LoginPage()
+
+    before(() => {
+        // runs once before all tests
+        cy.fixture('example.json').then(function(data)
+        {
+            this.data = data
+        }
+        )
+
+      })
+
+it('Chatbot - scrivi ad un operatore', function()
+{
+    Cypress.config('defaultCommandTimeout', 10000)
+    cy.visit(Cypress.env("url_bs"))
+    loginPage.getEntraButton().click()
+    loginPage.getUsernameTextField().type("minopica@gmail.com")
+    loginPage.getContinuaButton().click()
+    loginPage.getPasswordTextField().type('a12345678B')
+    loginPage.getAccediButton().click()
+    cy.wait(10000)
+    cy.get('#will').click()
+    // switch to iframe
+    cy.wait(40000)
+    cy.frameLoaded('#willie')
+    
+    cy.iframe().find('input[type="text"]').type('reset password')
+
+}
+)
+
+
+}
+)
