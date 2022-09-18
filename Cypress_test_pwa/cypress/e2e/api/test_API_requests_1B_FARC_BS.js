2fd0568ecf78f481918bd2b1267b9aac4e691953
Antimo Pica <antimo.pica@windtre.it>
2022-09-18 20:33:33 +0200

    aggiunta retrieve-mpd

--- Cypress_test_pwa/cypress/e2e/api/test_API_requests_1B_FARC_BS.js
+++ Cypress_test_pwa/cypress/e2e/api/test_API_requests_1B_FARC_BS.js
@@ -175,7 +175,6 @@ describe('1B FARC API - DEV - Test API', function()
             
             it.skip('api/ob/v2/contract/lineunfolded', function()
             {
-                console.log('Valore variabile ambiente SKIP_E2E_TESTS dentro blocco it() lineUnfolded: '+ Cypress.env("SKIP_E2E_TESTS"))
                 //skip esecuzione test se il flag SKIP_E2E_TESTS è a true.
                 cy.onlyOn(!Cypress.env("SKIP_E2E_TESTS"))
                 var token = ''
@@ -322,7 +321,6 @@ describe('1B FARC API - DEV - Test API', function()
 
                     it('api/v1/ch/payment/bills/events', function()
                     {
-                        console.log('Valore variabile ambiente SKIP_E2E_TESTS dentro blocco it() lineUnfolded: '+ Cypress.env("SKIP_E2E_TESTS"))
                         //skip esecuzione test se il flag SKIP_E2E_TESTS è a true.
                         cy.onlyOn(!Cypress.env("SKIP_E2E_TESTS"))
                         var token = ''
@@ -503,8 +501,7 @@ describe('1B FARC API - DEV - Test API', function()
                                             bodyRequest_bills.cdfList[i].lines[y].current = false
                                         }
                                     }
-        
-        
+
             
                                 })
                             
@@ -513,6 +510,97 @@ describe('1B FARC API - DEV - Test API', function()
                             }
                         )
 
+                        it('api/v1/ch/payment/retrieve-mdp', function()
+                        {
+                            console.log('Valore variabile ambiente SKIP_E2E_TESTS dentro blocco it() lineUnfolded: '+ Cypress.env("SKIP_E2E_TESTS"))
+                            //skip esecuzione test se il flag SKIP_E2E_TESTS è a true.
+                            cy.onlyOn(!Cypress.env("SKIP_E2E_TESTS"))
+                            var token = ''
+                            var codice_cliente = ''
+                            var lineId = ''
+                            var contractId = ''
+                            var flagFarc = ''
+                            var paymentType = ''
+                            var cdf = ''
+                            var contractStatus = ''
+                            var wasTied = ''
+                            var bodyRequest_bills = ''
+                            var indice = ''
+            
+                            cy.task('loadToken').then((jwt) => {
+                                console.log('token jwt: ' + jwt)
+                                token = jwt
+                                cy.task('loadArrayContracts').then((contratti)=> {
+                                    bodyRequest_bills = {"cdfList":[],"customerId":""}
+                                    console.log('array Contratti caricato: ' + contratti)
+                                    //console.log('lineId caricato da arrayContratti: '+ contratti[0].lines[0].id)
+                                    console.log('numero contratti: '+ contratti.length)
+                                    for (let i=0;i<contratti.length;i++) {
+                                        flagFarc = contratti[i].lines[0].flagFarc
+                                        console.log(`flagFarc linea n.${i}: ` + flagFarc)
+                                        if (flagFarc=='N') continue
+                                        console.log(`lineId n.${i}: ` + contratti[i].lines[0].id)
+                                        lineId = contratti[i].lines[0].id
+                                        console.log(`contractId n.${i}: ` + contratti[i].lines[0].contractId)
+                                        contractId = contratti[i].lines[0].contractId
+                                        console.log(`codice cliente linea n.${i}: ` + contratti[i].lines[0].customerId)
+                                        codice_cliente = contratti[i].lines[0].customerId
+                                        cdf = contratti[i].lines[0].idBillingAccount
+                                        console.log(`CDF linea n.${i}: ` + cdf)
+                                        wasTied = contratti[i].lines[0].wasTied
+                                        console.log(`wasTied linea n.${i}: ` + wasTied)
+                                        paymentType = contratti[i].lines[0].paymentType
+                                        console.log(`paymentType linea n.${i}: ` + paymentType)
+                                        contractStatus = contratti[i].status
+                                        console.log(`contractStatus linea n.${i}: ` + contractStatus)
+        
+                                        
+                                        if (bodyRequest_bills.cdfList.find((el) => el.cdf==cdf)) {
+                                            indice = bodyRequest_bills.cdfList.findIndex((el) => el.cdf==cdf)
+                                            console.log('*************** indice elemento cdf:'+ indice)
+                                            bodyRequest_bills.cdfList[indice].lines.push({"paymentType":paymentType,"id":lineId,"contractStatus":contractStatus,"wasTied":wasTied,"current": false})
+                                        }
+                                        else {
+                                            bodyRequest_bills.cdfList.push({"cdf":cdf, "lines":[{"paymentType":paymentType,"id":lineId,"contractStatus":contractStatus,"wasTied":wasTied,"current": false}]})
+                                        }
+                                        console.log('stampo oggetto bodyRequest_bills dentro ciclo', bodyRequest_bills)
+                                    }
+                                    console.log('lunghezza array cdfList: ' + bodyRequest_bills.cdfList.length)
+                                    if (bodyRequest_bills.cdfList.length == 0) {
+                                        //nessuna linea ha flagFarc a Y
+                                        console.log('*****entro nella condizione per skippare test*****')
+                                        return cy.onlyOn(false)
+                                    }
+                                    for (let i=0;i<bodyRequest_bills.cdfList.length;i++) {
+                                        bodyRequest_bills.customerId = codice_cliente
+                                        const requestObj = {
+                                            method: 'GET',
+                                            url: Cypress.env("base_url_bs") + '/api/v0/ch/payment/retrieve-mdp',
+                                            headers: {
+                                                Authorization: 'Bearer ' + `${token}`,
+                                                'Customer-Id': `${codice_cliente}`,
+                                                'X-Wind-Client': 'app-ios',
+                                                'X-Brand': 'ONEBRAND',
+                                                'Content-Type': 'application/json'
+                                            },
+                                            qs: {
+                                                "cdf": `${cdf}`
+                                            }
+                                        }
+                                        cy.requestAndLog(requestObj).then((resp) => {
+                                            expect(resp.status).to.eq(200)
+                                            expect(resp.body.status).to.be.equal('OK')
+                                            cy.logResponse(resp,requestObj.url)
+                                        })
+                                    }
+
+            
+                                })
+                            
+                                })
+                                
+                            }
+                        )    
 
             })
     })
