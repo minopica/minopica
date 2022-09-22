493c3b2c40ec2541edd7a253554f9487031c1b8e
Antimo Pica <antimo.pica@windtre.it>
2022-09-22 18:44:51 +0200

    aggiunti test playwright

---
+++ PlayWrightCourse/tests/UIBasicstest_dropdown-menu.spec.js
@@ -0,0 +1,27 @@
+const {test, expect} = require('@playwright/test')
+// browser e page sono fixture visibili a livello globale, vanno passate tra {}
+
+test('UI Dropdown Playwright test', async ({page})=>
+{
+    //chrome - plugins - cookies - context
+
+    const userName = page.locator('#username')
+    const signIn = page.locator('#signInBtn')
+    const password = page.locator('#password')
+    const dropdown = page.locator('select.form-control')
+    const cardTitles = page.locator(".card-body a")
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    await userName.fill("rahulshettyacademy")
+    await password.type("learning")
+    await dropdown.selectOption("consult")
+    // radio button
+    await page.locator('input[value="admin"]').click()
+    await page.locator('input[value="user"]').click()
+    await page.locator('#okayBtn').click()
+
+    //assert radio button user is selected
+    await page.pause()
+
+
+})
