083a0e2775f9008842fa411268b839366e2cffa4
antimo <>
2022-09-26 19:11:38 +0200

    aggiunti test in playwright da corso

--- PlayWrightCourse/tests/TestDemo.spec.js
+++ PlayWrightCourse/tests/TestDemo.spec.js
@@ -61,7 +61,44 @@ test.only('Page Playwright test', async ({page})=>
         }
     }
 
+    // assert sul testo contenuto nell'input textField
      const label = (await page.locator(".user__name input[type$='text']").inputValue())
      expect(label===email).toBeTruthy
-    await page.pause()
+    
+     // altro meccanismo di assertion sul testo
+     // await expect(page.locator(".user__name input[type$='text']")).toHaveText(email)
+
+     // click su PLACE ORDER
+    await page.locator('.action__submit').click()
+
+    // assert su thank you page
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ")
+    const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent()
+    console.log(orderId)
+
+    
+    // click su order history
+    await page.locator("label[routerlink='/dashboard/myorders']").click()
+
+    // await page.pause()
+    // cicla orderId della tabella alla ricerca dell'orderId
+    
+    const arrayOrderId = page.locator('tbody tr')
+    // l'action count su un selettore non ha l'auto wait in playwright, occorre aggiungere waitFor()
+    await arrayOrderId.nth(0).waitFor()
+    const arrayOrderId_count = await arrayOrderId.count()
+    console.log('Numero elementi array ordini in storico: '+arrayOrderId_count)
+    for(let i=0;i<arrayOrderId_count;i++) {
+        const text = await arrayOrderId.nth(i).locator('th').textContent()
+        console.log(text)
+        if (orderId.includes(text)) {
+            console.log('Ordine trovato in posizione: '+i)
+            await arrayOrderId.nth(i).locator('button').first().click()
+            break
+        }
+    }
+    
+    const orderID_finale = await page.locator('.col-text').textContent()
+    expect(orderId.includes(orderID_finale)).toBeTruthy()
+    //await page.pause()
 })
